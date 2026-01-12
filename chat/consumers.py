3532bb014a90709ff79a93726d47efa44ac32fff
FakeCoder01 <ayonsamajder@gmail.com>
2026-01-12 09:50:54 +0700

    new

--- chat/consumers.py
+++ chat/consumers.py
@@ -6,66 +6,66 @@ from .models import Chat
 from urllib.parse import parse_qsl
 
 
-
-
 class ChatConsumer(WebsocketConsumer):
-
     def connect(self):
-        query_params = dict(parse_qsl(self.scope['query_string'].decode('utf-8')))
-        group = query_params['group']
+        query_params = dict(parse_qsl(self.scope["query_string"].decode("utf-8")))
+        group = query_params["group"]
         if Profile.objects.filter(uid=group).exists():
-            self.room_group_name = str(group).replace('-', '')
+            self.room_group_name = str(group).replace("-", "")
 
             async_to_sync(self.channel_layer.group_add)(
-                self.room_group_name,
-                self.channel_name
+                self.room_group_name, self.channel_name
             )
             self.accept()
 
     def receive(self, text_data):
         text_data_json = json.loads(text_data)
 
-        message = text_data_json['message']
-        chat_group_id = text_data_json['chat_group_id']
-        sender = text_data_json['sender']        
+        message = text_data_json["message"]
+        chat_group_id = text_data_json["chat_group_id"]
+        sender = text_data_json["sender"]
 
         if not Profile.objects.filter(uid=chat_group_id).exists():
             return
 
-        if sender != 'user' and sender != 'manager':
-            return    
+        if sender != "user" and sender != "manager":
+            return
 
-        new_chat_msg =  Chat.objects.create(
-            profile = Profile.objects.get(uid=chat_group_id),
-            sender = sender,
-            message = message,
+        new_chat_msg = Chat.objects.create(
+            profile=Profile.objects.get(uid=chat_group_id),
+            sender=sender,
+            message=message,
         )
-
 
         async_to_sync(self.channel_layer.group_send)(
             self.room_group_name,
             {
-                'type':'chat_message',
-                'message':message,
-                'chat_group_id' : chat_group_id,
-                'sender' : sender,
-            }
+                "type": "chat_message",
+                "message": message,
+                "chat_group_id": chat_group_id,
+                "sender": sender,
+                "time_ago": new_chat_msg.time_ago,
+            },
         )
 
     def chat_message(self, event):
-        message = event['message']
-        chat_group_id = event['chat_group_id']
-        sender = event['sender']
+        message = event["message"]
+        chat_group_id = event["chat_group_id"]
+        sender = event["sender"]
 
         if not Profile.objects.filter(uid=chat_group_id).exists():
             return
-        if sender != 'user' and sender != 'manager':
+        if sender != "user" and sender != "manager":
             return
 
-        self.send(text_data=json.dumps({
-            'type':'chat',
-            'message':message,
-            'chat_group_id' : chat_group_id,
-            'sender' : sender,
-        }))
-
+        self.send(
+            text_data=json.dumps(
+                {
+                    "type": "chat",
+                    "message": message,
+                    "chat_group_id": chat_group_id,
+                    "sender": sender,
+                    "time_ago": event["time_ago"],
+                }
+            )
+        )
