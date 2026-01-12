0d04977444eab6e9f92d511b2e82e3d248355d75
Morishima Senpai <97719684+FakeCoder01@users.noreply.github.com>
2026-01-12 10:15:29 +0700

    Update admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,16 +1,3 @@
 # from django.contrib import admin
 
 # Register your models here.
-
-
-
-import cv2
-from PIL import ImageGrab
-
-
-
-while True:
-    img = ImageGrab.grab(bbox=(100, 10, 400, 780))
-    
-    cv2.imshow("Video :", cv2.cvtColor(img, cv2.COLOR_BGR2GRAY))
-    cv2.waitKey(1)
